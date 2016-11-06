9e00cfaa0d4a51cb7c2f304d263ba20171d5dd1f
Kmotiko <mottii.gen@gmail.com>
2016-11-06 19:48:42 +0900

    adjust number of jobs in test code

--- test/command_test.py
+++ test/command_test.py
@@ -32,7 +32,7 @@ class CommandTest(unittest.TestCase):
         assert llvmenv_main.main() == True
         del logging.getLogger('llvmenv').handlers[:]
 
-        sys.argv=['llvmenv', 'install', 'RELEASE_370.final', '--opt=-DCMAKE_CXX_COMPILER=clang++-3.6 -DCMAKE_C_COMPILER=clang-3.6']
+        sys.argv=['llvmenv', 'install', 'RELEASE_370.final', '--jobs', '4', '--opt=-DCMAKE_CXX_COMPILER=clang++-3.6 -DCMAKE_C_COMPILER=clang-3.6']
         assert llvmenv_main.main() == True
         del logging.getLogger('llvmenv').handlers[:]
 
